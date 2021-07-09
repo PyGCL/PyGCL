013a3962e2f3630ead1d93e165921c4c36e92c0f
Yichen Xu <yichen.x@outlook.com>
2021-07-09 19:40:19 +0800

    Update experiment script

--- exp.py
+++ exp.py
@@ -14,6 +14,7 @@ class Objective(Enum):
     JSD = 'JSD'
     Triplet = 'TM'
     Mixup = 'Mixup'
+    BL = 'BL'
 
 
 class Mode(Enum):
@@ -54,6 +55,7 @@ def resolve_param_path(dataset: str, mode: Mode, objective: Objective):
         Objective.Mixup: '',
         Objective.JSD: '_jsd',
         Objective.Triplet: '_triplet',
+        Objective.BL: '_bl',
     }
     param_dir = mode_dir[mode]
     param_name = dataset_param[dataset]
@@ -114,25 +116,53 @@ class GCLJob(Job):
                 Mode.GlobalGlobal: 'train_graph_g2g.py',
             }
         }
+        train_script_bl = {
+            TaskType.NodeTask: {
+                Mode.LocalLocal: 'train_node_BGRL_l2l_norm.py',
+                Mode.LocalGlobal: 'train_node_BGRL_g2l.py',
+            },
+            TaskType.GraphTask: {
+                Mode.LocalLocal: 'train_graph_BGRL_l2l.py',
+                Mode.LocalGlobal: 'train_graph_BGRL_g2l.py',
+                Mode.GlobalGlobal: 'train_graph_BGRL_g2g.py'
+            }
+        }
         additional_params = []
         for k, v in self.params.items():
             additional_params.append(f'--{k}')
             additional_params.append(f'{v}')
-        return [
-            Command(
-                exe="python",
-                args=[
-                    train_script[self.task_type][self.mode],
-                    "--dataset", self.dataset,
-                    "--param_path", self.param_path,
-                    "--loss", objective_name[self.objective],
-                    "--aug1", self.aug1, "--aug2", self.aug2,
-                    *additional_params,
-                    "--device", f"cuda:{args.gpus[worker_id % len(args.gpus)]}"
-                ],
-                stdout_redirect=self.log_path
-            )
-        ]
+
+        if self.objective != Objective.BL:
+            return [
+                Command(
+                    exe="python",
+                    args=[
+                        train_script[self.task_type][self.mode],
+                        "--dataset", self.dataset,
+                        "--param_path", self.param_path,
+                        "--loss", objective_name[self.objective],
+                        "--aug1", self.aug1, "--aug2", self.aug2,
+                        *additional_params,
+                        "--device", f"cuda:{args.gpus[worker_id % len(args.gpus)]}"
+                    ],
+                    stdout_redirect=self.log_path
+                )
+            ]
+        else:
+            return [
+                Command(
+                    exe="python",
+                    args=[
+                        train_script_bl[self.task_type][self.mode],
+                        "--dataset", self.dataset,
+                        "--param_path", self.param_path,
+                        "--aug1", self.aug1, "--aug2", self.aug2,
+                        *additional_params,
+                        "--device", f"cuda:{args.gpus[worker_id % len(args.gpus)]}"
+                    ],
+                    stdout_redirect=self.log_path
+                )
+            ]
 
 
 registered_runners = dict()
@@ -228,7 +258,6 @@ def mixup_s_wiki():
     return jobs
 
 
-
 @register_runner
 def mixup_threshold_s_imdb_multi():
     jobs = []
@@ -247,6 +276,37 @@ def mixup_threshold_s_imdb_multi():
                         param_path='params/GlobalGRACE/imdb_multi_triplet.json',
                         exp_name='mixup_threshold_s_ablation', mixup_threshold=threshold, mixup_s=s)
                 )
+
+    return jobs
+
+
+@register_runner
+def coauthor_phy():
+    jobs = []
+
+    topo_augs = ['ORI', 'EA', 'ER', 'EA+ER', 'ND', 'RWS']
+    feat_augs = ['ORI']
+    for i in range(3):
+        for topo_aug in topo_augs:
+            for feat_aug in feat_augs:
+                jobs.append(
+                    GCLJob(
+                        'Coauthor-Phy',
+                        Mode.LocalLocal, Objective.InfoNCE,
+                        topo_aug, feat_aug,
+                        i, exp_name='coauthor_phy'))
+
+    topo_augs = ['ER', 'ND']
+    feat_augs = ['FM', 'FD']
+    for i in range(3):
+        for topo_aug in topo_augs:
+            for feat_aug in feat_augs:
+                jobs.append(
+                    GCLJob(
+                        'Coauthor-Phy',
+                        Mode.LocalLocal, Objective.InfoNCE,
+                        topo_aug, feat_aug,
+                        i, exp_name='coauthor_phy'))
 
     return jobs
 
@@ -271,21 +331,49 @@ def mixup_threshold_s_computers():
 @register_runner
 def tau_ablation():
     jobs = []
-    taus = [0.05, 3]
+    taus = [0.05, 0.25, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3]
     for i in range(3):
         for tau in taus:
-            jobs.append(
-                GCLJob(
-                    'Coauthor-CS',
-                    Mode.LocalLocal, Objective.InfoNCE,
-                    'ER', 'FM',
-                    i, exp_name='tau_ablation', tau=tau))
             # jobs.append(
             #     GCLJob(
-            #         'IMDB-MULTI',
+            #         'Coauthor-CS',
             #         Mode.LocalLocal, Objective.InfoNCE,
             #         'ER', 'FM',
             #         i, exp_name='tau_ablation', tau=tau))
+            jobs.append(
+                GCLJob(
+                    'PROTEINS',
+                    Mode.LocalLocal, Objective.InfoNCE,
+                    'ER', 'FM',
+                    i, exp_name='tau_ablation', tau=tau))
+    return jobs
+
+
+@register_runner
+def obj_mode_reddit_binary():
+    jobs = []
+
+    modes = [
+        Mode.LocalLocal,
+        Mode.LocalGlobal,
+        Mode.GlobalGlobal,
+    ]
+    objectives = [
+        Objective.InfoNCE,
+        Objective.JSD,
+        Objective.Triplet,
+    ]
+    for i in range(1):
+        for obj in objectives:
+            for mode in modes:
+                jobs.append(
+                    GCLJob(
+                        'REDDIT-BINARY',
+                        mode, obj,
+                        'ER', 'FM', i,
+                        param_path='./params/GRACE/reddit_binary.json',
+                        exp_name='obj_mode_ablation'))
+
     return jobs
 
 
@@ -315,6 +403,154 @@ def bilevel_aug_ptc_mr():
                         Mode.LocalLocal, Objective.InfoNCE,
                         topo_aug, feat_aug,
                         i, exp_name='bilevel_aug'))
+
+    return jobs
+
+
+@register_runner
+def er_ea_sensitivity():
+    jobs = []
+
+    probs = [0.01, 0.05, 0.1, 0.2, 0.5, 0.6, 0.7, 0.8]
+    for i in range(10):
+        for prob in probs:
+            jobs.append(GCLJob('Coauthor-CS', Mode.LocalLocal, Objective.InfoNCE, 'ND', 'FM', i,
+                               exp_name='ER_EA_ablation',
+                               drop_node_prob1=prob,
+                               drop_node_prob2=prob))
+            jobs.append(GCLJob('Coauthor-CS', Mode.LocalLocal, Objective.InfoNCE, 'ER', 'FM', i,
+                               exp_name='ER_EA_ablation',
+                               drop_edge_prob1=prob,
+                               drop_edge_prob2=prob))
+            jobs.append(GCLJob('Coauthor-CS', Mode.LocalLocal, Objective.InfoNCE, 'EA', 'FM', i,
+                               exp_name='ER_EA_ablation',
+                               add_edge_prob1=prob,
+                               add_edge_prob2=prob))
+
+    return jobs
+
+
+@register_runner
+def bl_loss_graph():
+    jobs = []
+
+    datasets = ['IMDB-MULTI', 'PTC_MR', 'PROTEINS', 'REDDIT-BINARY']
+    modes = [Mode.LocalLocal, Mode.LocalGlobal]
+    for i in range(10):
+        for dataset in datasets:
+            for mode in modes:
+                jobs.append(GCLJob(dataset, mode, Objective.BL, 'ER', 'FM', i,
+                                   exp_name='bl_loss'))
+
+    return jobs
+
+
+@register_runner
+def bilevel_aug_feat_node():
+    jobs = []
+
+    datasets = [
+        # 'WikiCS', 'Coauthor-CS',
+        'Coauthor-Phy',
+        # 'Amazon-Computers',
+        # 'PTC_MR', 'PROTEINS',
+        # 'REDDIT-BINARY',
+        # 'IMDB-MULTI'
+    ]
+    topo_augs = ['EA', 'RWS']
+    feat_augs = ['FM', 'FD']
+    for i in range(10):
+        for dataset in datasets:
+            for topo_aug in topo_augs:
+                for feat_aug in feat_augs:
+                    jobs.append(
+                        GCLJob(
+                            dataset,
+                            Mode.LocalLocal, Objective.InfoNCE,
+                            topo_aug, feat_aug,
+                            i, exp_name='rebuttal_bilevel_aug'
+                        )
+                    )
+
+    return jobs
+
+
+@register_runner
+def bilevel_aug_feat_graph():
+    jobs = []
+
+    datasets = [
+        # 'WikiCS', 'Coauthor-CS',
+        # 'Coauthor-Phy',
+        # 'Amazon-Computers',
+        # 'PTC_MR', 'PROTEINS',
+        'REDDIT-BINARY',
+        # 'IMDB-MULTI'
+    ]
+    topo_augs = ['EA', 'RWS']
+    feat_augs = ['FM', 'FD']
+    for i in range(10):
+        for dataset in datasets:
+            for topo_aug in topo_augs:
+                for feat_aug in feat_augs:
+                    jobs.append(
+                        GCLJob(
+                            dataset,
+                            Mode.LocalLocal, Objective.InfoNCE,
+                            topo_aug, feat_aug,
+                            i, exp_name='bi-level-augmentations'
+                        )
+                    )
+
+    return jobs
+
+
+@register_runner
+def compositional_random_augmentations():
+    jobs = []
+
+    datasets = [
+        'WikiCS', 'Coauthor-CS', 'Amazon-Computers',
+        'PTC_MR', 'PROTEINS', 'REDDIT-BINARY', 'IMDB-MULTI'
+    ]
+    base_augs = ['PPR', 'MKD']
+    stoc_augs = ['EA', 'ND']
+    for i in range(10):
+        for dataset in datasets:
+            for base_aug in base_augs:
+                for stoc_aug in stoc_augs:
+                    jobs.append(
+                        GCLJob(
+                            dataset,
+                            Mode.LocalLocal, Objective.InfoNCE,
+                            base_aug, stoc_aug,
+                            i, exp_name='compositional-random-augmentations'
+                        )
+                    )
+
+    return jobs
+
+
+@register_runner
+def bl_batch_norm_ablation():
+    jobs = []
+
+    norms = ['none', 'batch']
+
+    for i in range(10):
+        for encoder_norm in norms:
+            for projector_norm in norms:
+                for predictor_norm in norms:
+                    job = GCLJob(
+                        'Coauthor-CS',
+                        Mode.LocalLocal, Objective.BL,
+                        topo_aug='FM', feat_aug='ER',
+                        repeat_id=i, exp_name='bl-batch-norm-ablation',
+                        encoder_norm=encoder_norm,
+                        projector_norm=projector_norm,
+                        predictor_norm=predictor_norm
+                    )
+                    jobs.append(job)
 
     return jobs
 
