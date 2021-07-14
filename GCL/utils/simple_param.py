aa446c15e1697de6c2d893ea65f017f97793410a
Yichen Xu <yichen.x@outlook.com>
2021-07-14 21:36:22 +0800

    Fix SP

--- GCL/utils/simple_param.py
+++ GCL/utils/simple_param.py
@@ -24,6 +24,18 @@ class SimpleParam:
         return json.loads(content)
 
     @staticmethod
+    def _mixin(orig_dict: dict, new_dict: dict):
+        for k, new_v in new_dict.items():
+            if k not in orig_dict:
+                orig_dict[k] = new_v
+            else:
+                orig_v = orig_dict[k]
+                if type(orig_v) is dict and type(new_v) is dict:
+                    orig_v.update(new_v)
+                else:
+                    orig_dict[k] = new_v
+
+    @staticmethod
     def _process_namespace(pre_loaded: dict, allowed_namespace: Optional[list] = None):
         namespace = []
         loaded = {}
@@ -32,15 +44,16 @@ class SimpleParam:
             ks = k.split(':')
             if len(ks) > 1:
                 classname = ks[0]
+                subkey = ks[1]
                 namespace.append(classname)
-                if classname in loaded and type(loaded[classname]) is list:
-                    loaded[classname].append(v)
+                if classname in loaded and type(loaded[classname]) is dict:
+                    loaded[classname][subkey] = v
                 else:
-                    loaded[classname] = [v]
+                    loaded[classname] = {subkey: v}
             else:
                 if type(v) is dict:
                     namespace.append(k)
-                loaded.update({k: v})
+                SimpleParam._mixin(loaded, {k: v})
 
         if allowed_namespace is not None:
             # Filter out not wanted namespaces
@@ -49,7 +62,7 @@ class SimpleParam:
         return namespace, loaded
 
     def __init__(self, default: Optional[dict] = None, allowed_namespaces: Optional[list] = None):
-        self.parameters = default if default is not None else dict()
+        default = default if default is not None else dict()
         namespace, loaded = self._process_namespace(default, allowed_namespaces)
         self.namespace = namespace
         self.parameters = loaded
@@ -61,12 +74,17 @@ class SimpleParam:
         loaded = {k: v for k, v in self.parameters.items() if k in allowed_namespaces}
         return loaded
 
+    def load(self, preloaded: dict, allowed_namespaces: Optional[List] = None, *args, **kwargs):
+        namespace, loaded = self._process_namespace(preloaded, allowed_namespaces)
+        self.namespace = list(set(self.namespace).union(namespace))
+        self._mixin(self.parameters, loaded)
+
     def update(self, from_: str, allowed_namespaces: Optional[list] = None, *args, **kwargs):
         if from_ == 'nni':
             loaded = nni.get_next_parameter()
-            namespace, loaded = self._process_namespace(loaded, allowed_namespaces)
-            self.parameters.update(loaded)
+            self.load(loaded, allowed_namespaces)
         else:
+            loaded = dict()
             if from_.endswith('.json'):
                 loaded = self._parse_json(from_)
             elif from_.endswith('.yaml') or from_.endswith('.yml'):
@@ -75,7 +93,18 @@ class SimpleParam:
             if 'preprocess_nni' in kwargs and kwargs['preprocess_nni']:
                 loaded = self._preprocess_nni(loaded)
 
-            namespace, loaded = self._process_namespace(loaded, allowed_namespaces)
+            self.load(loaded, allowed_namespaces)
 
-            self.namespace = list(set(self.namespace).union(namespace))
-            self.parameters.update(loaded)
+
+if __name__ == '__main__':
+    sp = SimpleParam()
+    print(sp.parameters)
+
+    sp.load({'foo': {'bar': 1}})
+    print(sp.parameters)
+
+    sp.load({'foo:baz': 2})
+    print(sp.parameters)
+
+    sp.load({'foo:bar': '1', 'foo:baz': 3, 'foo': {'quux': 4}})
+    print(sp.parameters)
