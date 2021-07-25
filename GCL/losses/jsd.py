4446c6fb8e7af5dfbc086bd91659b90b80637786
SXKDZ <mr.sxkdz@gmail.com>
2021-07-25 23:22:29 +0800

    Add Sampler

--- GCL/losses/jsd.py
+++ GCL/losses/jsd.py
@@ -4,8 +4,7 @@ import numpy as np
 import torch.nn.functional as F
 
 
-def jsd_loss(anchor, sample, discriminator, pos_mask, neg_mask=None,
-             include_intraview_negs=False, *args, **kwargs):
+def jsd_loss(anchor, sample, discriminator, pos_mask, neg_mask=None, *args, **kwargs):
     if neg_mask is None:
         neg_mask = 1 - pos_mask
     num_neg = neg_mask.int().sum()
@@ -17,12 +16,6 @@ def jsd_loss(anchor, sample, discriminator, pos_mask, neg_mask=None,
 
     neg_similarity = similarity * neg_mask
     E_neg = (F.softplus(- neg_similarity) + neg_similarity - np.log(2)).sum()
-    if include_intraview_negs:
-        intra_similarity = discriminator(anchor, anchor)
-        intra_neg_mask = 1 - torch.eye(anchor.shape[0], device=intra_similarity.device)
-        intra_neg_similarity = intra_similarity * intra_neg_mask
-        E_neg += (F.softplus(- intra_neg_similarity) + intra_neg_similarity - np.log(2)).sum()
-        num_neg += intra_neg_mask.int().sum()
     E_neg /= num_neg
 
     return E_neg - E_pos
@@ -39,8 +32,8 @@ class JSDLoss(torch.nn.Module):
         if batch is None:
             if h3 is None and h4 is None:  # same-scale contrasting
                 anchor, sample, pos_mask, neg_mask = self.sampler(anchor=h1, sample=h2)
-                return jsd_loss(anchor, sample, discriminator=self.discriminator, pos_mask=pos_mask, neg_mask=neg_mask,
-                                include_intraview_negs=self.include_intraview_negs, *args, **kwargs)
+                return jsd_loss(anchor, sample, discriminator=self.discriminator,
+                                pos_mask=pos_mask, neg_mask=neg_mask, *args, **kwargs)
             else:  # global to local, only one graph
                 anchor1, sample1, pos_mask1, neg_mask1 = self.sampler(anchor=g1, sample=h2, neg_sample=h4)
                 anchor2, sample2, pos_mask2, neg_mask2 = self.sampler(anchor=g2, sample=h1, neg_sample=h3)
@@ -48,28 +41,6 @@ class JSDLoss(torch.nn.Module):
             anchor1, sample1, pos_mask1, neg_mask1 = self.sampler(anchor=g1, sample=h2, batch=batch)
             anchor2, sample2, pos_mask2, neg_mask2 = self.sampler(anchor=g2, sample=h1, batch=batch)
 
-        l1 = jsd_loss(anchor1, sample1, self.discriminator, pos_mask=pos_mask1, neg_mask=neg_mask1,
-                      include_intraview_negs=self.include_intraview_negs, *args, **kwargs)
-        l2 = jsd_loss(anchor2, sample2, self.discriminator, pos_mask=pos_mask2, neg_mask=neg_mask2,
-                      include_intraview_negs=self.include_intraview_negs, *args, **kwargs)
-        return l1 + l2
-
-
-class JSDLossG2L(torch.nn.Module):
-    def __init__(self, discriminator):
-        super(JSDLossG2L, self).__init__()
-        self.discriminator = discriminator
-
-    def forward(self, h1, g1, h2, g2, batch, *args, **kwargs):
-        num_graphs = g1.shape[0]
-        num_nodes = h1.shape[0]
-        device = h1.device
-
-        pos_mask = torch.zeros((num_nodes, num_graphs)).to(device)
-        for node_idx, graph_idx in enumerate(batch):
-            pos_mask[node_idx][graph_idx] = 1.
-
-        l1 = jsd_loss(g2, h1, self.discriminator, pos_mask=pos_mask.t(), *args, **kwargs)
-        l2 = jsd_loss(g1, h2, self.discriminator, pos_mask=pos_mask.t(), *args, **kwargs)
-
+        l1 = jsd_loss(anchor1, sample1, self.discriminator, pos_mask=pos_mask1, neg_mask=neg_mask1, *args, **kwargs)
+        l2 = jsd_loss(anchor2, sample2, self.discriminator, pos_mask=pos_mask2, neg_mask=neg_mask2, *args, **kwargs)
         return l1 + l2
--- GCL/samplers/samplers.py
+++ GCL/samplers/samplers.py
@@ -2,11 +2,32 @@ import torch
 from torch_scatter import scatter
 
 
-class SameScaleSampler(torch.nn.Module):
-    def __init__(self):
-        super(SameScaleSampler, self).__init__()
+class Sampler:
+    def __init__(self, intraview_negs=False):
+        self.intraview_negs = intraview_negs
 
-    def forward(self, anchor, sample):
+    def __call__(self, anchor, sample, *args, **kwargs):
+        return self.sample(anchor, sample, *args, **kwargs)
+
+    def sample(self, anchor, sample, *args, **kwargs):
+        pass
+
+    def add_intraview_negs(self, anchor, sample, pos_mask, neg_mask):
+        num_nodes = anchor.size(0)
+        device = anchor.device
+        intraview_pos_mask = torch.zeros_like(pos_mask, device=device)
+        intraview_neg_mask = torch.ones_like(pos_mask, device=device) - torch.eye(num_nodes, device=device)
+        new_sample = torch.cat([sample, anchor], dim=0)                     # (M+N) * K
+        new_pos_mask = torch.cat([pos_mask, intraview_pos_mask], dim=1)     # M * (M+N)
+        new_neg_mask = torch.cat([neg_mask, intraview_neg_mask], dim=1)     # M * (M+N)
+        return anchor, new_sample, new_pos_mask, new_neg_mask
+
+
+class SameScaleSampler(Sampler):
+    def __init__(self, *args, **kwargs):
+        super(SameScaleSampler, self).__init__(*args, **kwargs)
+
+    def sample(self, anchor, sample, *args, **kwargs):
         assert anchor.size(0) == sample.size(0)
         num_nodes = anchor.size(0)
         device = anchor.device
@@ -15,11 +36,11 @@ class SameScaleSampler(torch.nn.Module):
         return anchor, sample, pos_mask, neg_mask
 
 
-class CrossScaleSampler(torch.nn.Module):
+class CrossScaleSampler(Sampler):
     def __init__(self):
         super(CrossScaleSampler, self).__init__()
 
-    def forward(self, anchor, sample, batch=None, neg_sample=None, use_gpu=True):
+    def sample(self, anchor, sample, batch=None, neg_sample=None, use_gpu=True, *args, **kwargs):
         num_graphs = anchor.shape[0]  # M
         num_nodes = sample.shape[0]   # N
         device = sample.device
@@ -42,3 +63,4 @@ class CrossScaleSampler(torch.nn.Module):
 
         neg_mask = 1. - pos_mask
         return anchor, sample, pos_mask, neg_mask
+
