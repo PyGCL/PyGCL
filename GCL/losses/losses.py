0d744140d33fb01febf2ebb939d5d1a9dd17bd7e
SXKDZ <mr.sxkdz@gmail.com>
2021-07-26 23:43:26 +0800

    Add loss interface

---
+++ GCL/losses/losses.py
@@ -0,0 +1,12 @@
+import torch
+from abc import ABC, abstractmethod
+
+
+class Loss(ABC):
+    @abstractmethod
+    def __compute(self, anchor, sample, pos_mask, neg_mask, *args, **kwargs) -> torch.FloatTensor:
+        pass
+
+    def __call__(self, anchor, sample, pos_mask, neg_mask, *args, **kwargs) -> torch.FloatTensor:
+        loss = self.__compute(anchor, sample, pos_mask, neg_mask, *args, **kwargs)
+        return loss
