75c1eaf4e0665115c9a5ad019f9e83906a476314
SXKDZ <mr.sxkdz@gmail.com>
2021-08-18 15:39:52 +0800

    Add DGI transductive

---
+++ examples/DGI.py
@@ -0,0 +1,91 @@
+import torch
+import os.path as osp
+import GCL.losses as L
+import torch_geometric.transforms as T
+
+from tqdm import tqdm
+from torch import nn
+from GCL.eval import get_split, LREvaluator
+from GCL.models import SingleBranchContrastModel
+from torch_geometric.nn import GCNConv
+from torch_geometric.nn.inits import uniform
+from torch_geometric.datasets import Planetoid
+
+
+class GConv(nn.Module):
+    def __init__(self, input_dim, hidden_dim, activation, num_layers):
+        super(GConv, self).__init__()
+        self.activation = activation()
+        self.layers = torch.nn.ModuleList()
+        self.layers.append(GCNConv(input_dim, hidden_dim))
+        for _ in range(num_layers - 1):
+            self.layers.append(GCNConv(hidden_dim, hidden_dim))
+
+    def forward(self, x, edge_index, edge_weight=None):
+        z = x
+        for i, conv in enumerate(self.layers):
+            z = conv(z, edge_index, edge_weight)
+            z = self.activation(z)
+        return z
+
+
+class Encoder(torch.nn.Module):
+    def __init__(self, encoder, hidden_dim):
+        super(Encoder, self).__init__()
+        self.encoder = encoder
+        self.project = torch.nn.Linear(hidden_dim, hidden_dim)
+        uniform(hidden_dim, self.project.weight)
+
+    @staticmethod
+    def corruption(x, edge_index):
+        return x[torch.randperm(x.size(0))], edge_index
+
+    def forward(self, x, edge_index):
+        z = self.encoder(x, edge_index)
+        g = self.project(torch.sigmoid(z.mean(dim=0, keepdim=True)))
+        zn = self.encoder(*self.corruption(x, edge_index))
+        return z, g, zn
+
+
+def train(encoder_model, contrast_model, data, optimizer):
+    encoder_model.train()
+    optimizer.zero_grad()
+    z, g, zn = encoder_model(data.x, data.edge_index)
+    loss = contrast_model(h=z, g=g, hn=zn)
+    loss.backward()
+    optimizer.step()
+    return loss.item()
+
+
+def test(encoder_model, data):
+    encoder_model.eval()
+    z, _, _ = encoder_model(data.x, data.edge_index)
+    split = get_split(num_samples=z.size()[0], train_ratio=0.1, test_ratio=0.8)
+    result = LREvaluator()(z, data.y, split)
+    return result
+
+
+def main():
+    device = torch.device('cuda')
+    path = osp.join(osp.expanduser('~'), 'datasets')
+    dataset = Planetoid(path, name='Cora', transform=T.NormalizeFeatures())
+    data = dataset[0].to(device)
+
+    gconv = GConv(input_dim=dataset.num_features, hidden_dim=32, activation=torch.nn.PReLU, num_layers=2).to(device)
+    encoder_model = Encoder(encoder=gconv, hidden_dim=32).to(device)
+    contrast_model = SingleBranchContrastModel(loss=L.JSDLoss(), mode='G2L')
+
+    optimizer = torch.optim.Adam(encoder_model.parameters(), lr=0.01)
+
+    with tqdm(total=1000, desc='(T)') as pbar:
+        for epoch in range(1, 1001):
+            loss = train(encoder_model, contrast_model, data, optimizer)
+            pbar.set_postfix({'loss': loss})
+            pbar.update()
+
+    test_result = test(encoder_model, data)
+    print(f'(E): Best test F1Mi={test_result["micro_f1"]:.4f}, F1Ma={test_result["macro_f1"]:.4f}')
+
+
+if __name__ == '__main__':
+    main()
