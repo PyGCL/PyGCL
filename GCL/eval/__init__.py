eadb152a55bbcccbbd2863a4a79acca2b3574427
SXKDZ <mr.sxkdz@gmail.com>
2021-08-21 00:13:53 +0800

    Add predefined splits

--- GCL/eval/__init__.py
+++ GCL/eval/__init__.py
@@ -1,13 +1,16 @@
-from .eval import BaseEvaluator, BaseSKLearnEvaluator, get_split
+from .eval import BaseEvaluator, BaseSKLearnEvaluator, get_split, from_predefined_split
 from .logistic_regression import LREvaluator
 from .svm import SVMEvaluator
+from .random_forest import RFEvaluator
 
 __all__ = [
     'BaseEvaluator',
     'BaseSKLearnEvaluator',
     'LREvaluator',
     'SVMEvaluator',
-    'get_split'
+    'RFEvaluator',
+    'get_split',
+    'from_predefined_split'
 ]
 
 classes = __all__
