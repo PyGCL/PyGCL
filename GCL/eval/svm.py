bf3f187d75eeaee06e0f1a30a1e9a091e661dd57
SXKDZ <Mr.SXKDZ@Gmail.com>
2021-12-13 23:34:22 +0800

    Update svm and example

--- GCL/eval/svm.py
+++ GCL/eval/svm.py
@@ -1,13 +1,33 @@
+from typing import Union, Callable, List, Optional, Dict
 from sklearn.svm import LinearSVC, SVC
+from sklearn.model_selection import BaseCrossValidator
+
 from GCL.eval import BaseSKLearnEvaluator
 
 
 class SVMEvaluator(BaseSKLearnEvaluator):
-    def __init__(self, linear=True, param_grid=None, cv=None, metric=None, refit=None):
+    """
+    Evaluation using SVM.
+
+    Parameters:
+        linear (bool): Whether to use linear SVM. (default: :obj:`True`)
+        metrics (Dict[str, Callable]): The metric(s) to evaluate.
+        split (BaseCrossValidator): The sklearn cross-validator, used to split the data.
+        param_grid (Optional[List[Dict]], optional): The parameter grid for the grid search. (default: :obj:`None`)
+        grid_search_scoring (Dict[str, Callable]): The metric(s) used in grid search. (default: :obj:`None`)
+        cv_params (Optional[Dict], optional): Pass the parameters for the sklearn cross-validator.
+         See sklearn :obj:`GridSearchCV<https://scikit-learn.org/stable/modules/
+         generated/sklearn.model_selection.GridSearchCV.html>`_ for details. (default: :obj:`None`)
+    """
+    def __init__(
+            self, metrics: Dict[str, Callable], split: BaseCrossValidator,
+            linear=True, param_grid: Optional[Dict] = None,
+            grid_search_scoring: Optional[Dict[str, Callable]] = None,
+            cv_params: Optional[Dict] = None):
         if linear:
             self.evaluator = LinearSVC()
         else:
             self.evaluator = SVC()
-        if param_grid is None:
-            param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
-        super(SVMEvaluator, self).__init__(self.evaluator, param_grid, cv, metric, refit)
+        super(SVMEvaluator, self).__init__(
+            evaluator=self.evaluator, metrics=metrics, split=split,
+            param_grid=param_grid, grid_search_scoring=grid_search_scoring, cv_params=cv_params)
