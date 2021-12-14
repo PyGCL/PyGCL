21bc3cce73331fd723b9511dc4ea4ca0cae17a79
SXKDZ <Mr.SXKDZ@Gmail.com>
2021-12-14 11:35:02 +0800

    Add sklearn logistic regression model

--- GCL/eval/logistic_regression.py
+++ GCL/eval/logistic_regression.py
@@ -1,15 +1,19 @@
 import torch
+
 from tqdm import tqdm
 from torch import nn
+from typing import Union, List, Callable, Dict, Optional
 from torch.optim import Adam
 from sklearn.metrics import f1_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import BaseCrossValidator
 
-from GCL.eval import BaseEvaluator
+from GCL.eval import BaseEvaluator, BaseSKLearnEvaluator
 
 
-class LogisticRegression(nn.Module):
+class LRModel(nn.Module):
     def __init__(self, num_features, num_classes):
-        super(LogisticRegression, self).__init__()
+        super(LRModel, self).__init__()
         self.fc = nn.Linear(num_features, num_classes)
         torch.nn.init.xavier_uniform_(self.fc.weight.data)
 
@@ -32,7 +36,7 @@ class LREvaluator(BaseEvaluator):
         input_dim = x.size()[1]
         y = y.to(device)
         num_classes = y.max().item() + 1
-        classifier = LogisticRegression(input_dim, num_classes).to(device)
+        classifier = LRModel(input_dim, num_classes).to(device)
         optimizer = Adam(classifier.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
         output_fn = nn.LogSoftmax(dim=-1)
         criterion = nn.NLLLoss()
@@ -78,3 +82,30 @@ class LREvaluator(BaseEvaluator):
             'micro_f1': best_test_micro,
             'macro_f1': best_test_macro
         }
+
+
+class LRSklearnEvaluator(BaseSKLearnEvaluator):
+    """
+    Evaluation using the sklearn logistic regression classifier.
+
+    Parameters:
+        metrics (Dict[str, Callable]): The metric(s) to evaluate.
+        split (BaseCrossValidator): The sklearn cross-validator to split the data.
+        params (Dict, optional): Other parameters for the logistic regression model.
+         See sklearn :obj:`LogisticRegression<https://scikit-learn.org/stable/modules/generated/
+         sklearn.linear_model.LogisticRegression.html>`_ for details. (default: :obj:`None`)
+        param_grid (List[Dict], optional): The parameter grid for the grid search. (default: :obj:`None`)
+        grid_search_scoring (Dict[str, Callable], optional):
+         If :obj:`param_grid` is given, provide metric(s) in grid search. (default: :obj:`None`)
+        cv_params (Dict, optional): If :obj:`param_grid` is given, further pass the parameters
+         for the sklearn cross-validator. See sklearn :obj:`GridSearchCV<https://scikit-learn.org/stable/modules/
+         generated/sklearn.model_selection.GridSearchCV.html>`_ for details. (default: :obj:`None`)
+    """
+    def __init__(
+            self, metrics: Dict[str, Callable], split: BaseCrossValidator,
+            params: Optional[Dict] = None, param_grid: Optional[Dict] = None,
+            grid_search_scoring: Optional[Dict[str, Callable]] = None,
+            cv_params: Optional[Dict] = None):
+        super(LRSklearnEvaluator, self).__init__(
+            evaluator=LogisticRegression(), metrics=metrics, split=split, params=params,
+            param_grid=param_grid, grid_search_scoring=grid_search_scoring, cv_params=cv_params)
