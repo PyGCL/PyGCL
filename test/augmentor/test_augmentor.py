0140a7b596882058a800f48d0be9ae66eadff8de
Yichen Xu <yichen.x@outlook.com>
2022-01-18 23:25:45 +0800

    Add tests for EdgeAdding

--- test/augmentor/test_augmentor.py
+++ test/augmentor/test_augmentor.py
@@ -5,6 +5,7 @@ import torch_geometric.transforms as T
 from functools import partial
 from torch_geometric.data import Data
 from GCL.augmentor import PyGAugmentor, DGLAugmentor, Compose
+from GCL.augmentor import EdgeAdding
 
 
 def test_pygaugmentor():
@@ -38,3 +39,22 @@ def test_composing_augmentor():
     comp_aug = Compose([aug1, aug2])
     data = comp_aug(data)
     assert data.x.tolist() == [[1.0, 2.0], [1.0, 2.0], [1.0, 2.0]]
+
+
+def test_edge_adding():
+    aug = EdgeAdding(pe=0.8)
+    edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
+    data = Data(edge_index=edge_index, num_nodes=3)
+    aug_data = aug(data)
+    assert aug_data.edge_index.shape[0] == 2
+    assert aug_data.edge_index.shape[1] >= data.edge_index.shape[1]
+
+
+def test_edge_adding_dgl():
+    aug = EdgeAdding(pe=0.8)
+    edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
+    u, v = edge_index
+    g: dgl.DGLGraph = dgl.graph((u, v), num_nodes=3)
+    aug_g = aug(g)
+
+    assert aug_g.num_edges() >= g.num_edges()
