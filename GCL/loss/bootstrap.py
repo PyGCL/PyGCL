33c7d1c53c6e0f5458122ecad59cd69f9e1d4af6
Liang Wang <wangliang.leon20@gmail.com>
2022-01-23 12:02:43 +0800

    Update default bootstrap loss

--- GCL/loss/bootstrap.py
+++ GCL/loss/bootstrap.py
@@ -19,12 +19,9 @@ class BootstrapLatent(Loss):
 
     def compute_default_positive(self, contrast_instance: ContrastInstance, *args, **kwargs) -> torch.FloatTensor:
         anchor, sample = contrast_instance.anchor, contrast_instance.sample
-        num_nodes = anchor.size(0)
-        device = anchor.device
-        pos_mask = torch.eye(num_nodes, dtype=torch.float32, device=device)
         anchor = F.normalize(anchor, dim=-1, p=2)
         sample = F.normalize(sample, dim=-1, p=2)
 
         similarity = anchor @ sample.t()
-        loss = (similarity * pos_mask).sum(dim=-1)
+        loss = similarity.diag()
         return loss.mean()
